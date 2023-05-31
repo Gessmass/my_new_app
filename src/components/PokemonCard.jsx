c4b62b85cf3b9dc9e4dc72361adb647a8d3a283c
Gessmass <martinweilldrums@gmail.com>
2023-05-31 14:53:55 +0200

    react 5

--- src/components/PokemonCard.jsx
+++ src/components/PokemonCard.jsx
@@ -1,13 +1,25 @@
 
+import PropTypes from 'prop-types';
+
+
+
+
 function PokemonCard (props) {
     console.log(props.pokemon);
-
+    
     return (
         <figure>
-            {props.pokemon.imgSrc} != undefined? <img src={props.pokemon.imgSrc} alt={props.pokemon.name} /> : <p>???</p>
+            {props.pokemon.imgSrc != undefined? <img src={props.pokemon.imgSrc} alt={props.pokemon.name} /> : <p>???</p>}
             <figcaption>{props.pokemon.name}</figcaption>
         </figure>
     );
+}
+
+PokemonCard.propTypes = {
+    props: PropTypes.shape({
+        name: PropTypes.string.isRequired,
+        imgSrc: PropTypes.string,
+    }).isRequired,
 }
 
 export default PokemonCard;
