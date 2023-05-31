c7a6fac5f463fa15b19553ebc3309ef550243589
Gessmass <martinweilldrums@gmail.com>
2023-05-31 12:02:54 +0200

    fin quete 4

--- src/App.jsx
+++ src/App.jsx
@@ -7,6 +7,17 @@ import PokemonCard from './components/PokemonCard'
 function App() {
   const [count, setCount] = useState(0)
 
+  const pokemonList = [
+    {
+        name: "bulbasaur",
+        imgSrc:
+        "https://raw.githubusercontent.com/PokeAPI/sprites/master/sprites/pokemon/other/official-artwork/1.png",
+    },
+    {
+        name: "mew",
+    },
+];
+
   return (
     <>
       <div>
@@ -25,7 +36,7 @@ function App() {
         </p>
       </div>
       <div>
-        <PokemonCard />
+        <PokemonCard pokemon = {pokemonList[0]} />
       </div>
       <p className="read-the-docs">
         Click on the Vite and React logos to learn more
