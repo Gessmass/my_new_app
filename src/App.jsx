09462e9487eba659c9507e23d167e4717a969dd8
Gessmass <martinweilldrums@gmail.com>
2023-05-31 10:56:46 +0200

    bulbausaur

--- src/App.jsx
+++ src/App.jsx
@@ -2,7 +2,7 @@ import { useState } from 'react'
 import reactLogo from './assets/react.svg'
 import viteLogo from '/vite.svg'
 import './App.css'
-import Dino from './components/PokemonCard'
+import PokemonCard from './components/PokemonCard'
 
 function App() {
   const [count, setCount] = useState(0)
@@ -10,12 +10,10 @@ function App() {
   return (
     <>
       <div>
-        <a href="https://vitejs.dev" target="_blank">
           <img src={viteLogo} className="logo" alt="Vite logo" />
-        </a>
-        <a href="https://react.dev" target="_blank">
+      
           <img src={reactLogo} className="logo react" alt="React logo" />
-        </a>
+        
       </div>
       <h1>Vite + React</h1>
       <div className="card">
@@ -27,7 +25,7 @@ function App() {
         </p>
       </div>
       <div>
-        <Dino />
+        <PokemonCard />
       </div>
       <p className="read-the-docs">
         Click on the Vite and React logos to learn more
--- src/components/PokemonCard.jsx
+++ src/components/PokemonCard.jsx
@@ -1,10 +1,24 @@
-function Dino () {
+
+
+const pokemonList = [
+    {
+        name: "bulbasaur",
+        imgSrc:
+        "https://raw.githubusercontent.com/PokeAPI/sprites/master/sprites/pokemon/other/official-artwork/1.png",
+    },
+    {
+        name: "mew",
+    },
+];
+
+function PokemonCard () {
+    const pokemon = pokemonList[0];
+
     return (
-    <figure>
-        <img src="https://raw.githubusercontent.com/PokeAPI/sprites/master/sprites/pokemon/other/official-artwork/1.png"/>
-        <figcaption>bulbausaur</figcaption>
+        <figure>
+            {pokemon.imgSrc != undefined ? <img src={pokemon.imgSrc} alt={pokemon.name} /> : <p>???</p>}
+            <figcaption>{pokemon.name}</figcaption>
         </figure>
     );
 }
-
-export default Dino;
+export default PokemonCard;
